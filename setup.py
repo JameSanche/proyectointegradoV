0609193d991923999b02027fcdca8c119c074edb
James Sánchez Toro <james.sanchez@est.iudigital.edu.co>
2025-05-06 22:20:45 +0000

    Carga inicial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='proyectointegradoV',
+    version='0.1',
+    author='James Sanchez',
+    author_email="",
+    description='Proyecto Integrado V',
+    py_modules=['activity1'],
+    install_requires=[
+        'pandas',
+        'openpyxl',
+        'requests',
+        'pandas',
+    ],
+
+)
