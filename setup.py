acffb2500d30e64b581981cd08c740da2fad2e6d
JameSanche <james.sanchez@est.iudigital.edu.co>
2025-05-25 02:37:54 -0500

    update files

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup, find_packages
+
 setup(
     name='proyectointegradoV',
     version='0.1',
     author='James Sanchez',
-    author_email="",
     description='Proyecto Integrado V',
-    py_modules=['activity1'],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         'pandas',
         'openpyxl',
         'requests',
-        'pandas',
         'beautifulsoup4',
+        'scikit-learn',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'statsmodels'
     ],
-
 )
